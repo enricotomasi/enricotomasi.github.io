7b1c8c8f8924335ab8a407b09fcd3cf03d49dd6c
Enrico Tomasi <enricotomasi@outlook.com>
2026-01-11 14:37:47 +0100

    commit

--- RaccattaMonitor/index.service.worker.js
+++ RaccattaMonitor/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1768137772|1045069189';
+const CACHE_VERSION = '1768138616|1888313698';
 /** @type {string} */
 const CACHE_PREFIX = 'RaccattaMonitorG-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
