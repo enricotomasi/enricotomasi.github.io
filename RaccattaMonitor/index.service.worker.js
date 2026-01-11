4d5ce0721c635792f9e80a09475162d03e6373d4
Enrico Tomasi <enricotomasi@outlook.com>
2026-01-11 11:20:02 +0100

    Update index.service.worker.js

--- RaccattaMonitor/index.service.worker.js
+++ RaccattaMonitor/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1767170396|57664021';
+const CACHE_VERSION = '1768126676|714391657';
 /** @type {string} */
 const CACHE_PREFIX = 'New Game Project-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
